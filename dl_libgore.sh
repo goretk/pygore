ac5c4d183d847d30274fcf4e84b6c62cae56119e
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2019-08-24 22:43:26 +0100

    Use xargs

--- dl_libgore.sh
+++ dl_libgore.sh
@@ -4,9 +4,9 @@ RELEASE_URL=https://api.github.com/repos/goretk/libgore/releases/latest
 
 mkdir -p dltmp
 
-for i in `curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4`; do
-   echo "Downloading $i"
-   curl -sL $i | bsdtar -xvf - -C dltmp
+for i in linux darwin windows; do
+   curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep $i | \
+      xargs -n 1 -I url curl -sL url | bsdtar -xvf - -C dltmp
 done
 
 cp -v dltmp/*/{*.dll,*.so,*.dylib} pygore/.
