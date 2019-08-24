cbf3640ce1a0faf096b1bd548911228487c742f4
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2019-08-24 22:38:29 +0100

    Ticks

--- dl_libgore.sh
+++ dl_libgore.sh
@@ -4,7 +4,7 @@ RELEASE_URL=https://api.github.com/repos/goretk/libgore/releases/latest
 
 mkdir -p dltmp
 
-for i in $(curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4); do
+for i in `curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4`; do
    echo "Downloading $i"
    curl -sL $i | bsdtar -xvf - -C dltmp
 done
