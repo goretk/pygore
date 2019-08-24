f09d8a1d6f2126145a326830289b841156519a82
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2019-08-24 22:54:55 +0100

    Add darwin and windows

--- dl_libgore.sh
+++ dl_libgore.sh
@@ -1,16 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash
 
 RELEASE_URL=https://api.github.com/repos/goretk/libgore/releases/latest
 LINUX_URL=`curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep linux`
+DARWIN_URL=`curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep darwin`
+WINDOWS_URL=`curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep windows`
 
 mkdir -p dltmp
 
 curl -sL $LINUX_URL | bsdtar -xvf - -C dltmp
-
-
-# for i in linux darwin windows; do
-#    curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep $i | \
-#       xargs -n 1 -I url curl -sL url | bsdtar -xvf - -C dltmp
-# done
+curl -sL $DARWIN_URL | bsdtar -xvf - -C dltmp
+curl -sL $WINDOWS_URL | bsdtar -xvf - -C dltmp
 
 cp -v dltmp/*/{*.dll,*.so,*.dylib} pygore/.
