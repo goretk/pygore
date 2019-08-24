1478fa0bc880e0018892b4ce9ce376319f1b70d9
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2019-08-24 22:49:48 +0100

    No loop

--- dl_libgore.sh
+++ dl_libgore.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -x
 
 RELEASE_URL=https://api.github.com/repos/goretk/libgore/releases/latest
+LINUX_URL=`curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep linux`
 
 mkdir -p dltmp
 
-for i in linux darwin windows; do
-   curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep $i | \
-      xargs -n 1 -I url curl -sL url | bsdtar -xvf - -C dltmp
-done
+curl -sL $LINUX_URL | bsdtar -xvf - -C dltmp
+
+
+# for i in linux darwin windows; do
+#    curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep $i | \
+#       xargs -n 1 -I url curl -sL url | bsdtar -xvf - -C dltmp
+# done
 
 cp -v dltmp/*/{*.dll,*.so,*.dylib} pygore/.
