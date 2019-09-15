c69b56abc8401fc4e99d09fce0b190d27bd082e9
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2019-09-15 22:16:01 +0100

    Bump version for Go 1.13 detection

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pygore',
-    version='0.2.1',
+    version='0.2.2',
     author="Go Reverse Engineering Tool Kit",
     description="Python bindings for the Go Reverse Engineering Tool Kit",
     long_description=long_description,
