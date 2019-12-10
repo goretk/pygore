ce16a856b3afde6ab1b19e4aa8c3cb7de1d10ea3
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2019-12-10 21:35:10 +0000

    Set release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pygore',
-    version='0.3.2',
+    version='0.3.3',
     author="Go Reverse Engineering Tool Kit",
     description="Python bindings for the Go Reverse Engineering Tool Kit",
     long_description=long_description,
